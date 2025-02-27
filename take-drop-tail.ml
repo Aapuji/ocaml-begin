404944c0e9f253734ffc4488ada0dfdfecf35357
Aapuji <om.robotics16@gmail.com>
2025-02-27 16:33:47 -0500

    Added doc comment

--- take-drop-tail.ml
+++ take-drop-tail.ml
@@ -1,11 +1,11 @@
-(* returns:  [from i j l] is the list containing the integers from
+(** returns:  [from i j l] is the list containing the integers from
  *   [i] to [j], inclusive, followed by the list [l].
  * example:  [from 1 3 [0] = [1;2;3;0]] *)
  let rec from i j l =
   if i > j then l
   else from i (j - 1) (j :: l)
 
-(* returns:  [i -- j] is the list containing the integers from
+(** returns:  [i -- j] is the list containing the integers from
  *   [i] to [j], inclusive.
  *) 
 let (--) i j =
